f5299620136ffb9ede0e325cab5791d3da866fc6
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-01 00:45:53 +0000

    Use a loop to filter the data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,7 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str):
     """Obfuscate fileds inside a message"""
-    field = '|'.join(fields)
-    pattern = fr'({field})=[^{separator}]*'
-    return re.sub(pattern, r'\1=' + redaction, message)
+    for field in fields:
+        message = re.sub(fr'{field}=[^{separator}]*',
+                         fr'{field}={redaction}', message)
+    return message
