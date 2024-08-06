cc4c7439e29b8b3a1a44b5bf97984c56b6a4fa12
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-06 23:14:11 +0000

    Extract user credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -29,3 +29,12 @@ class BasicAuth(Auth):
                                     validate=True).decode('utf-8')
         except binascii.Error:
             None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """Extract the username and password"""
+        if (decoded_base64_authorization_header is None
+                or type(decoded_base64_authorization_header) is not str
+                or ':' not in decoded_base64_authorization_header):
+            return None, None
+        return tuple(decoded_base64_authorization_header.split(':'))
