3b145a978e9ef469671a2cc13eeb202c4ec62f56
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-06 23:02:21 +0000

    Decode a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """The Basic Auth class"""
+import binascii
+import base64
 from .auth import Auth
 
 
@@ -15,3 +17,15 @@ class BasicAuth(Auth):
         if fields[0] != 'Basic':
             return None
         return fields[1]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Return the decoded value of a Base64 string"""
+        if (base64_authorization_header is None
+                or type(base64_authorization_header) is not str):
+            return None
+        try:
+            return base64.b64decode(base64_authorization_header,
+                                    validate=True).decode('utf-8')
+        except binascii.Error:
+            None
