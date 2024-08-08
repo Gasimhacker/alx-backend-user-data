ca050e1a97cb8c018ddc0d21d2cbb9684c5e2228
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-08 09:30:13 +0000

    Delete the user session / logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """ Module of Index views
 """
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.user import User
 from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login() -> str:
-    """ GET /api/v1/status
+def login():
+    """ Post /api/v1/auth_session/login
     Return:
-      - the status of the API
+      - The login user with a cookie
     """
     email = request.form.get('email')
     if email is None or email == '':
@@ -37,3 +37,16 @@ def login() -> str:
             res.set_cookie(cookie_name, session_id)
             return res
     return jsonify({"error": "wrong password"}), 401
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ DELETE /api/v1/auth_session/logout
+    Return:
+      - On delete: an empty JSON dictionary with the status code 200
+    """
+    from api.v1.app import auth
+    if not auth.destroy_session(request):
+        abort(404)
+    return jsonify({})
