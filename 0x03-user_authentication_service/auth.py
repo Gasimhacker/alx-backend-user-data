7d654ead53e3ff7c135d9b6277131cca2cafed2f
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-15 08:22:22 +0000

    Implement the Auth.get_user_from_session_id method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -42,6 +42,16 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> str | None:
+        """Find a user by his session id"""
+        if session_id is None:
+            return None
+        try:
+            u = self._db.find_user_by(session_id=session_id)
+            return u
+        except NoResultFound:
+            return None
+
 
 def _generate_uuid() -> str:
     """Generate a string representation of a new UUID"""
