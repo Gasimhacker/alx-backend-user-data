128790e0ae72f02916eaf6b5cf7474bb54337ac2
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-15 10:21:24 +0000

    Create the route that updates the password in the database

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -71,5 +71,18 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password():
+    """Rest the user's password"""
+    email = request.form['email']
+    reset_token = request.form['reset_token']
+    new_password = request.form['new_password']
+    try:
+        Auth.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"})
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
