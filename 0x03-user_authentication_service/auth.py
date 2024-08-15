0faa1229fd57764fd0dc548175ee6c7154df4584
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-15 07:33:08 +0000

    Implement credentials validation method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -14,7 +14,7 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """Register a nw user in the database"""
+        """Register a new user in the database"""
         try:
             u = self._db.find_user_by(email=email)
             raise ValueError(f'User {email} already exists')
@@ -22,6 +22,14 @@ class Auth:
             password = _hash_password(password)
             u = self._db.add_user(email, password)
             return u
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if a user with there credentials exists"""
+        try:
+            u = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode(), u.hashed_password)
+        except NoResultFound:
+            return False
 
 
 def _hash_password(password: str) -> bytes:
