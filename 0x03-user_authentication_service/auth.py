c204afcb69a9835867aaeee5219b07abfe02e39e
Mohamed Ahmed <ma2286572@gmail.com>
2024-08-15 07:44:58 +0000

    Remove the indentation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,9 +32,10 @@ class Auth:
         except NoResultFound:
             return False
 
-    def _generate_uuid() -> str:
-        """Generate a string representation of a new UUID"""
-        return str(uuid4())
+
+def _generate_uuid() -> str:
+    """Generate a string representation of a new UUID"""
+    return str(uuid4())
 
 
 def _hash_password(password: str) -> bytes:
